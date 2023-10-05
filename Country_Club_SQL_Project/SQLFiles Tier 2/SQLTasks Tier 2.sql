3873e585b3ace3439367ccacee991a38977e6e2a
tpoozhikala <57980120+tpoozhikala@users.noreply.github.com>
2023-10-05 12:11:57 -0400

    Reworking Question 6 SQLTasks Tier 2.sql

--- Country_Club_SQL_Project/SQLFiles Tier 2/SQLTasks Tier 2.sql
+++ Country_Club_SQL_Project/SQLFiles Tier 2/SQLTasks Tier 2.sql
@@ -79,10 +79,10 @@ ORDER BY monthlymaintenance DESC;
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
-SELECT firstname, surname AS lastname  
+SELECT firstname, surname AS lastname, joindate
 FROM Members
-WHERE firstname <> 'GUEST' AND surname <> ' GUEST'
-ORDER BY lastname;
+WHERE firstname <> 'GUEST' AND surname <> 'GUEST' 
+ORDER BY joindate DESC;
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
